b2b374130001a3f3f036dcda2e5f4f3dcf786fe3
derrick matheny <mathenyderrick@gmail.com>
2021-10-08 09:54:45 -0400

    users can create accounts and add themseles to database

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -13,20 +17,23 @@ def logout():
 def signup():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash("email must be greater than 3 characters", category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash("first name must be greater than 1 character", category='error')
         elif password1 != password2:
              flash("passwords do not match", category='error')
         elif len(password1) <7:
              flash("password must be greater than 6 characters", category='error')
         else:
+            new_user= User(email=email,first_name=first_name,password=generate_password_hash(password1, method='sha256'))
+            #add user to database
+            db.session.add(new_user)
+            db.session.commit()
             flash("account created!", category="success")
-            # pass #add user to database 
-
+            return redirect(url_for('views.home'))
     return render_template("signup.html")
